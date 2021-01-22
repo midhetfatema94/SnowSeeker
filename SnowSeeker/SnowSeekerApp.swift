2314ea25b2e0745d9baad86f9152ee175dd7c66b
Midhet Sulemani <midhetfatema94@gmail.com>
2021-01-22 14:06:05 +0300

    first commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Waveline Media on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
